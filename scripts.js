f5646f61d4df40463ee0e598f797be8f0b30eb85
Raspberry Pi Buenos Aires <38385411+PIBSAS@users.noreply.github.com>
2024-08-11 11:34:08 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,68 @@
 // Part of the CVTerminal class
 class CVTerminal {
+  terminal;
+  isAnimating;
+  command;
   // ... other properties and methods ...
+  constructor(config) {
+     this.config = config;
+     this.initializeProperties();
+     // Additional initialization
+   }
+  initializeProperties() {
+     this.terminal = new Terminal(this.config.terminal);
+     // Further initialization
+   }
+  handleKeyEvent({ key, domEvent }) {
+     // Logic for key event processing
+   }
 
+   handleCommand() {
+     // Logic for executing commands
+   }
+
+   handleInput(key) {
+     // Manage character inputs in the terminal
+   }
+
+   writePrompt() {
+     // Display command prompt
+   }
+  startFullCV() {
+     // Start full CV display
+   }
+
+   resetFullCV() {
+     // Reset CV display to initial state
+   }
+
+   animateTyping(text, pos, callback) {
+     // Typing effect for text
+   }
+
+   interruptAnimation() {
+     // Stop ongoing animations
+   }
+  const terminalSettings = {
+     fontSize: 9,
+     fontFamily: "'VT323', monospace",
+     // Additional settings
+   };
+
+   const cvInteraction = {
+     commands: ["about", "experience", "projects"],
+     // Other interactions
+   };
+  window.onload = () => {
+     const terminalConfigurations = {
+       terminal: terminalSettings,
+       cv: cvInteraction,
+       // More configurations
+     };
+     new CVTerminal(terminalConfigurations);
+   };
+  
+  
   // Method to handle the 'experience' command
   writeSection(sectionName) {
     if (sectionName === "experience") {
